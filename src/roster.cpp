25125e3528707af26b2a670996189602c68cb6ea
TobiWald <tt2x@yahoo.de>
2026-09-14 19:23:30 +0200

    Update roster.cpp

--- src/roster.cpp
+++ src/roster.cpp
@@ -2,17 +2,28 @@
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 
+// Eigene UTC-Umrechnung, da timegm() auf dieser ESP32-Toolchain nicht deklariert ist
+static time_t timegmManual(int year, int month0, int day, int hour, int min, int sec) {
+  static const int cumDays[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+  long days = (long)(year - 1970) * 365L
+            + (year - 1969) / 4 - (year - 1901) / 100 + (year - 1601) / 400
+            + cumDays[month0]
+            + (day - 1);
+  bool isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+  if (month0 > 1 && isLeap) days += 1;
+  return days * 86400L + hour * 3600L + min * 60L + sec;
+}
+
 // Parst ICS-Zeitformat "YYYYMMDDTHHMMSSZ" (immer UTC) in einen time_t-Wert
 static time_t parseIcsUtc(const String& s) {
   if (s.length() < 15) return 0;
-  struct tm t = {};
-  t.tm_year = s.substring(0, 4).toInt() - 1900;
-  t.tm_mon  = s.substring(4, 6).toInt() - 1;
-  t.tm_mday = s.substring(6, 8).toInt();
-  t.tm_hour = s.substring(9, 11).toInt();
-  t.tm_min  = s.substring(11, 13).toInt();
-  t.tm_sec  = s.substring(13, 15).toInt();
-  return timegm(&t);
+  int year  = s.substring(0, 4).toInt();
+  int month = s.substring(4, 6).toInt() - 1;
+  int day   = s.substring(6, 8).toInt();
+  int hour  = s.substring(9, 11).toInt();
+  int min   = s.substring(11, 13).toInt();
+  int sec   = s.substring(13, 15).toInt();
+  return timegmManual(year, month, day, hour, min, sec);
 }
 
 bool fetchRoster(const String& icsUrl, std::vector<FlightEvent>& outEvents) {
