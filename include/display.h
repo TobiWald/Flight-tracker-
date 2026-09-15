8878e764de6f4a235d6be578d11ea45abd884564
TobiWald <tt2x@yahoo.de>
2026-09-15 13:02:09 +0200

    Update display.h

--- include/display.h
+++ include/display.h
@@ -1,12 +1,6 @@
 #pragma once
-#include "roster.h"
-#include "flightstatus.h"
+#include <Arduino.h>
 
 void displayInit();
 void displayWifiSetup();
-void displayNoFlight();
-void displayFlightScheduled(const FlightEvent& f, time_t now);
-void displayFlightBoarding(const FlightEvent& f, int minutesToDeparture);
-void displayFlightDelayed(const FlightEvent& f, int minutesLate);
-void displayFlightAirborne(const FlightEvent& f, const LiveStatus& s, int delayMinutes);
-void displayFlightLanded(const FlightEvent& f);
+void displayMessage(const String& line1, const String& line2, uint32_t color);
