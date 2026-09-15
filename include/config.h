3a28c2fa842bb225843fa608c4e100d1c320fefb
TobiWald <tt2x@yahoo.de>
2026-09-15 16:24:35 +0200

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,22 +1,27 @@
 #pragma once
 
-// Display-Pins (ESP32-2424S012C / GC9A01 240x240 rund) - schon bekannt vom Plane-Radar-Projekt
-#define DISPLAY_PIN_RST   GPIO_NUM_NC
-#define DISPLAY_PIN_CS    GPIO_NUM_10
-#define DISPLAY_PIN_DC    GPIO_NUM_2
-#define DISPLAY_PIN_MOSI  GPIO_NUM_7
-#define DISPLAY_PIN_SCLK  GPIO_NUM_6
-#define DISPLAY_PIN_BL    GPIO_NUM_3
-#define DISPLAY_BL_PWM_CHANNEL 0
+// Display-Pins (Waveshare ESP32-S3-Touch-LCD-1.47, JD9853, ST7789-kompatibel angesteuert)
+#define LCD_PIN_DC   45
+#define LCD_PIN_CS   21
+#define LCD_PIN_SCK  38
+#define LCD_PIN_MOSI 39
+#define LCD_PIN_RST  40
+#define LCD_PIN_BL   46
+#define LCD_WIDTH    172
+#define LCD_HEIGHT   320
+#define LCD_COL_OFFSET1 34
+#define LCD_ROW_OFFSET1 0
+#define LCD_COL_OFFSET2 34
+#define LCD_ROW_OFFSET2 0
 
 // Name des WLAN-Setup-Access-Points beim ersten Start
 #define WIFI_AP_NAME "FlightTracker-Setup"
 
 // Zeitintervalle
-#define ROSTER_REFRESH_INTERVAL_MS   (15UL * 60UL * 1000UL)   // Dienstplan alle 15 Min neu laden
-#define ADSB_POLL_INTERVAL_ACTIVE_MS (60UL * 1000UL)          // Live-Status jede Minute im aktiven Fenster
-#define SCREEN_CYCLE_MS               (5UL * 1000UL)         // Bildschirm alle 5s wechseln (3 Screens im Kreis)
-#define DELAYED_THRESHOLD_MIN        15   // Minuten nach geplantem Abflug, ab der "verspätet" statt "startet gleich" angezeigt wird
-#define LANDED_BUFFER_AFTER_MIN      20   // Minuten nach geplanter Ankunft, ab der der Flug als beendet gilt
+#define ROSTER_REFRESH_INTERVAL_MS   (15UL * 60UL * 1000UL)
+#define ADSB_POLL_INTERVAL_ACTIVE_MS (60UL * 1000UL)
+#define AWAY_MESSAGE_SWITCH_MS       (5UL * 1000UL)
+#define DELAYED_THRESHOLD_MIN        15
+#define LANDED_BUFFER_AFTER_MIN      20
 
 #define ADSB_API_HOST "https://api.adsb.fi/v2/callsign/"
