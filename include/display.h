0ab15dd90c3806dcdca911407928247062b1203d
TobiWald <tt2x@yahoo.de>
2026-09-15 13:26:46 +0200

    Update display.h

--- include/display.h
+++ include/display.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <Arduino.h>
 
+// Eigene Farbkonstanten (RGB565), damit main.cpp nicht auf LovyanGFX-Header
+// zugreifen muss - die sind nur in display.cpp eingebunden
+#define COLOR_GREEN   0x07E0
+#define COLOR_SKYBLUE 0x867D
+#define COLOR_RED     0xF800
+#define COLOR_WHITE   0xFFFF
+
 void displayInit();
 void displayWifiSetup();
 void displayMessage(const String& line1, const String& line2, uint32_t color);
